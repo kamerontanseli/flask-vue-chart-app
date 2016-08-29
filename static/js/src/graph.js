0b99a6d7fc938ff94268243c78340c76361a439b
Kameron Tanseli <kameron@localhost.localdomain>
2016-08-29 11:44:32 +0100

    Fixed HEX RGB bug

--- static/js/src/graph.js
+++ static/js/src/graph.js
@@ -4,20 +4,10 @@
 
   Vue.config.delimiters = ['[[', ']]'];
 
-  function hexToRgb(hex) {
-      // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-      hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-          return r + r + g + g + b + b;
-      });
-
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? {
-          r: parseInt(result[1], 16),
-          g: parseInt(result[2], 16),
-          b: parseInt(result[3], 16)
-      } : null;
-  }
+  function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+  function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+  function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+  function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
 
   var chart_parent = new Vue({
       el: "#chart_parent",
@@ -45,8 +35,8 @@
                   label: this.label,
                   data: this.data.map(function(el){ return el.data; }),
                   backgroundColor: this.data.map(function(el){
-                      var rgb = hexToRgb(el.bck_color);
-                      return "rgba("+rgb.r+","+rgb.b+","+rgb.g+",0.5)";
+                      var c = el.bck_color;
+                      return "rgba("+hexToR(c)+","+hexToG(c)+","+hexToB(c)+",0.5)";
                   })
                 }]
               },
